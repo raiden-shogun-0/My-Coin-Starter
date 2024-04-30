3d72a70dbc15e758e04100b92f09628d5bda8078
Chatsopon Deepateep <chatsopon.d@lmwn.com>
2024-04-30 22:25:56 +0700

    Initial Commit

---
+++ My Coin/My_CoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_CoinApp.swift
+//  My Coin
+//
+//  Created by Chatsopon.d on 30/4/24.
+//
+
+import SwiftUI
+
+@main
+struct My_CoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
